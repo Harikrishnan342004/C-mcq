af06f4ecc115288835b1d7890a70f25c5bd62d8e
Hari <harikrishnan53828@gmail.com>
2024-02-09 15:22:41 +0530

    second

--- Helloworld.c
+++ Helloworld.c
@@ -1,3 +1,4 @@
+//1
 /*#include<stdio.h>
 int main()
 {
@@ -6,7 +7,7 @@ int main()
 	printf("%d", num);
 	return 0;
 }*/
-
+//2===============================================
 /*#include<stdio.h>
 int main()
 {
@@ -15,7 +16,7 @@ int main()
    int a = printf("%d",printf(" Cprog\n\n"));
    return 0;
 }*/
-
+//3===================================================
 /*#include<stdio.h>
 int main()
 {
@@ -25,6 +26,8 @@ arr[i]=200;
 printf("%d %d",6[arr],arr[1]);
 return 0;
 }  */
+
+//4===========
 
 // #include<stdio.h>
 // int main()
@@ -39,6 +42,8 @@ return 0;
 // return 0;
 // }
 
+//5================
+
 // #include<stdio.h>
 // int main()
 // {
@@ -52,6 +57,7 @@ return 0;
 // return 0;
 // }
 
+//6
 
 // #include<stdio.h>
 // int main()
@@ -66,6 +72,7 @@ return 0;
 // return 0;
 // }
 
+//7
 
 // #include<stdio.h>
 // int main()
@@ -75,11 +82,139 @@ return 0;
 // return 0;           //right to left 
 // }
 
-#include<stdio.h>
-int main ()
-{
-int a=10,b=2;
-int *pa=&a, *pb=&b;
-printf("value =%d", *pa/ *pb);
-return 0;
-}
+//8
+
+// #include<stdio.h>
+// int main ()
+// {
+// int a=10,b=2;
+// int *pa=&a, *pb=&b;
+// printf("value =%d", *pa/ *pb);
+// return 0;
+// }
+
+
+//9
+
+// #include<stdio.h>
+// int main()
+// {
+// 	int a , b , c;
+// 	printf("%d ", (scanf("%d %d %d", &a, &b, &c)));
+// 	return 0;
+// }
+
+//10
+
+// #include<stdio.h>
+// int main()
+// {
+// 	int d = 1000;
+// 	int a = printf("%d\n",d);
+// 	printf("%d", a);
+// 	return 0;
+// }
+
+//11
+
+// #include<stdio.h>
+// int main()
+// {
+// 	int a = 10;
+
+// 	if(a == 10)
+// 	{
+// 		printf(" c++ \n");
+// 		break;
+// 		printf(" c");
+// 	}
+// 	else
+// 	{
+// 		printf("java");
+// 	}
+// }
+
+//12
+
+// #include<stdio.h>
+// int main()
+// {
+// 	int a = 10;
+// while(a<11){
+// 	if(a == 10)
+// 	{
+// 		printf(" c++ \n");
+// 		break;
+// 		printf(" c");
+// 	}
+// 	else
+// 	{
+// 		printf("java");
+// 	}
+	
+// }
+// return 0;
+// }
+
+
+//13
+
+// #include <stdio.h>
+
+// int main() {
+//   int k = 0;
+//   while (k < 3, k = 0, k < 5, k = 1, k < 2) {
+//     printf("c prog \n ");
+//     k++;
+//   }
+
+//   return 0;
+// }
+
+//14
+
+// #include <stdio.h>
+// int main()
+// {
+// 	printf("%d", 10.2);
+// 	return 0;
+// }
+
+//15
+
+// #include<stdio.h>
+// int main()
+// {
+// int a = --10;
+// printf("%d",a);
+// return 0;
+// }
+
+//16
+
+// #include<stdio.h>
+// int main()
+// {
+// 	int d=1;
+// 	while(d++ < printf("cprog"))
+// 	{
+// 		printf("\n%d\n",d);
+//     }
+// }
+
+//17
+
+// #include<stdio.h>
+// int main()
+// {
+// int a;
+// for(a=1; a<=7; a++)
+// {
+// if(a < 4)
+// continue;
+// else
+// break;
+// printf("c prog");
+// }
+// printf("C++");
+// }
