e75470debc2f38553bffb40a79ababcd698d2772
Hari <harikrishnan53828@gmail.com>
2024-02-12 21:34:06 +0530

    kk

--- addtwonum.c
+++ addtwonum.c
@@ -1,9 +1,37 @@
-#include<stdio.h>
-int main()
-{
-    int x , y;
-    printf("Enter two number = \n");
-    scanf("%d%d", &x,&y);
-    int sum = x + y;
-    printf("%d", sum);
+#include <stdio.h>
+#include <string.h>
+
+void swap(char *x, char *y) {
+    char temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void permute(char *str, int l, int r, int *count) {
+    int i;
+    if (l == r) {
+        (*count)++;
+    } else {
+        for (i = l; i <= r; i++) {
+            swap((str + l), (str + i));
+            permute(str, l + 1, r, count);
+            swap((str + l), (str + i)); // backtrack
+        }
+    }
+}
+
+int main() {
+    char str[100];
+    int count = 0;
+    
+    printf("Enter a string: ");
+    scanf("%s", str);
+    
+    int n = strlen(str);
+    
+    permute(str, 0, n - 1, &count);
+    
+    printf("Output: %d\n", count);
+    
+    return 0;
 }
