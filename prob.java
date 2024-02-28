0434c6c38da2b6587042af23b3a8ace1b52729b8
Hari <harikrishnan53828@gmail.com>
2024-02-28 23:31:14 +0530

    hi

--- prob.java
+++ prob.java
@@ -1,17 +1,17 @@
-public int zoho(String str1, String str2) {
-    int num = 0;
+// public int zoho(String str1, String str2) {
+//     int num = 0;
     
-    for (int i = 0; i < str2.length(); i++) {
-        if (str1.indexOf(str2.charAt(i)) != -1) {
-            num++;
-        }
-    }
+//     for (int i = 0; i < str2.length(); i++) {
+//         if (str1.indexOf(str2.charAt(i)) != -1) {
+//             num++;
+//         }
+//     }
     
-    return num;
-}
+//     return num;
+// }
 
-// Example usage:
-String str1 = "ZohoForLife";
-String str2 = "HappyWorkHappyLife";
-int result = zoho(str1, str2);
-System.out.println(result);
+// // Example usage:
+// String str1 = "ZohoForLife";
+// String str2 = "HappyWorkHappyLife";
+// int result = zoho(str1, str2);
+// System.out.println(result);
